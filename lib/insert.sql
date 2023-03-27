5ec3aad0c789859307a8e2336ec89f5482429a95
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-03-26 21:30:10 -0600

    done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES 
+(1, "Mr.Chocolate", 12, 'M', 'Brown', 'rowdy', 1),
+(2, "Rowdy", 14, 'M', 'Brown', 'rowdy', 1),
+(3, "Tabitha", 11, 'M', 'Brown', 'rowdy', 1),
+(4, "Sergeant Brown", 3, 'M', 'Brown', 'rowdy', 1),
+(5, "Melissa", 4, 'M', 'Brown', 'rowdy', 1),
+(6, "Grinch", 5, 'M', 'Brown', 'rowdy', 1),
+(7, "Wendy", 7, 'M', 'Brown', 'rowdy', 1),
+(8, null, 8, 'M', 'Brown', 'rowdy', 1);
